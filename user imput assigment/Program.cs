5c84ae5cb61460feebd515b3a923445a61ba1b47
North Harrington <339713067@GoTVDSB.ca>
2024-09-24 09:39:08 -0400

    assigment

--- user imput assigment/Program.cs
+++ user imput assigment/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,14 +14,10 @@ namespace user_imput_assigment
         {
             //Part1();
             //North
-            Part2()
-            
-
-            
-            
-
-            
-
+            //Part2();
+            //Part3();
+            //Part4();
+            Part5();
         }
 
         public static void Part1()
@@ -57,7 +54,86 @@ namespace user_imput_assigment
             Console.WriteLine("What grade are you in?");
             Int32.TryParse(Console.ReadLine(), out grade);
             Console.WriteLine("What is your login name?");
-            loginname
+            loginname = Console.ReadLine();
+            Console.WriteLine("What is your student ID? ");
+            int.TryParse(Console.ReadLine(), out studentid);
+            Console.WriteLine("What is your average?");
+            double.TryParse(Console.ReadLine(), out average);
+
+
+        }
+
+        public static void Part3()
+        {
+            string name;
+            int age;
+
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Hey " + name + " how old are you?");
+            Int32.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine("Did you know that in 5 years you will be " + (age + 5));
+            Console.WriteLine(" and did you know that 5 years ago you were " + (age - 5));
+
+
+
+
+        }
+        public static void Part4()
+        {
+            double num1;
+            double num2;
+            double num3;
+
+            Console.WriteLine(" please enter your numbers");
+            double.TryParse(Console.ReadLine(), out num1);
+            double.TryParse(Console.ReadLine(), out num2);
+            double.TryParse(Console.ReadLine(), out num3);
+            Console.WriteLine("=" + (num1 + num2 + num3) / 2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        public static void Part5()
+        {
+            string item1;
+            string item2;
+            double price1;
+            double price2;
+            double subtotal;
+
+
+            Console.WriteLine("what two items whould you like?");
+            Console.WriteLine("item 1");
+            item1 = Console.ReadLine();
+            Console.WriteLine("item 2");
+            item2 = Console.ReadLine();
+
+            Console.WriteLine(" What is the price of item 1");
+            double.TryParse(Console.ReadLine(), out price1);
+            Console.WriteLine(" What is the price of item 2");
+            double.TryParse(Console.ReadLine(), out price2);
+            subtotal = price1 + price2;
+            Console.WriteLine(" The total of the two will be " + subtotal.ToString("C"));
+
 
 
 
@@ -74,6 +150,13 @@ namespace user_imput_assigment
 
         }
     }
+    
+
+
+
+
+
+
 
 
 
